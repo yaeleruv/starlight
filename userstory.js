3c6389542bc447f99645b981331bb05b80f8f3c9
Yael Eruv <yaeleruv@gmail.com>
2020-11-30 00:13:21 +0200

    initial commit

---
+++ userstory.js
@@ -0,0 +1,18 @@
+// The Museum site should be elegant and approachable to the user.
+// at the main page the user will see:
+// 1. a menu navbar 
+// 2. a few imges from the museum colection
+// 3. store/ shop - optional
+// 3. at the bottom of the page, contact details
+
+// >> threw the menu navbar the user can nevigate to: 
+// about
+// Gallery/ Colections?
+// Tickets (purches)
+// store/ shop - optional
+// Contact us
+
+
+
+// gallery > imgs-
+// manet1.jpg - ‘A Game of Croquet’ (1873).
